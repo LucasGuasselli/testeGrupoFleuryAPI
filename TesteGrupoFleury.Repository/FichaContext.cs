57a999a86509438578f366fbc1dd13301ab199a1
Lucas Guasselli <lucaszack265@gmail.com>
2021-05-05 21:56:31 -0300

    Adicionado ClienteController com um GET sem parâmetro e um GET({id}) testando cadastro no BD (apenas temporariamente)

--- TesteGrupoFleury.Repository/FichaContext.cs
+++ TesteGrupoFleury.Repository/FichaContext.cs
@@ -9,6 +9,11 @@ namespace testeGrupoFleuryAPI.Reporitory
 {
     public class FichaContext: DbContext
     {
+        public FichaContext()
+        {
+
+        }
+
         public FichaContext(DbContextOptions<FichaContext> options): base(options)
         {
 
@@ -16,7 +21,12 @@ namespace testeGrupoFleuryAPI.Reporitory
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Ficha> Fichas { get; set; }
         public DbSet<Unidade> Unidades { get; set; }
-        public DbSet<Item> Itens { get; set; }        
+        public DbSet<Item> Itens { get; set; }
 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=123456;Persist Security Info=True;User ID=sa;Initial Catalog=FichaApp;Data Source=GUASSELLI-PC\\SQLEXPRESS");
+        }
     }
 }
---
+++ testeGrupoFleuryAPI/Controllers/ClienteController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testeGrupoFleuryAPI.Dominio;
+using testeGrupoFleuryAPI.Reporitory;
+
+namespace testeGrupoFleuryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : Controller
+    {
+               
+       public readonly FichaContext _context;
+       public ClienteController(FichaContext context)
+       {
+          _context = context;
+       }
+
+       // GET: api/<ClienteController>
+       [HttpGet]
+       public ActionResult Get()
+       {
+           var Clientes = _context.Clientes.ToList();
+              return Ok(Clientes);
+       }
+
+        // GET: api/<ClienteController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var cliente = new Cliente { Nome = "Lucas"};
+            using (var contexto = new FichaContext())
+            {
+                contexto.Clientes.Add(cliente);
+
+                contexto.SaveChanges();
+            }
+            return Ok();
+        }
+
+    }
+}
